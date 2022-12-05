112a1eeb971fef92e6b0ce15102e22bcdde8bca8
erikbengtsson <eribeng@student.chalmers.se>
2022-12-05 10:52:08 +0100

    lab 3 push

--- lab3/README.md
+++ lab3/README.md
@@ -1,3 +1,53 @@
-#### Lab 3
+#### Lab 3.1 and 3.2
+
+The lab has been implemented by myself, but a lot of concepts and rulas has been discussed together with Karl Wennerström.
+
+I've also been using code provided by the professor during the lectures, mainly for part 3.1.
+
+# Description
+The player taking the last object wins.
+
+- Task 3.1: An agent using fixed rules based on nim-sum (i.e., an expert system)
+- Task 3.2: An agent using evolved rules
+- Task 3.3: An agent using minmax
+- Task 3.4: An agent using reinforcement learning
+
+### 3.1 - Expert system
+
+Most of this solution has been provided from professor in class, so that it feels unnecessary
+with to many comments. However, the solution uses the nim_sum and plays against a random opponent. As long as the nr 
+of rows isn't too few (like 2 or 1), it basically always wins against a player playing random.
+
+It's also possible for it to play other opponents but the hard coded game loop might have to be changed a bit.
 
 
+### 3.2 Evolved Rules
+The basic idea of the solution was to come up with some rules that several agents used in certain situations. These rules all
+have a parameter telling us how many tiles to leave on a row in the case of this being applied. The parameters are randomized in the 
+creation of the population and the idea is that the genes using the best tweaked parameters will win the most games and therefor become
+our best individuals.
+
+As an example, one of the rules is used when the game only consists of on active row. In this case, we obviuosly want to take all tiles on the last row 
+and win the game which means that the ideal parameter for that rule is zero.
+
+# Implementation 
+The solution uses a (lamdba, mu)-strategy.
+
+The current implementation is to use the provided cooked interface to find the cases on when to apply what rules. The agents playing is instances of 
+Gene class and holds information about themselves, but also has a Strategy for when making a move. The strategies are a strategy pattern which makes it possible
+to move with different concrete implementations.
+
+We start by creating a population with N number of Genes and then let them face a random gene 10 different times. After each game the genes stats are updated.
+Then two parents are selected using a tournament selection for crossover. Crossover in this case is basically just take parameter 1 from parent 1 and 2 from parent 2.
+
+Mutation happens with prob P and it selects a random individual from the population and randomizes its parameter. 
+
+The script currently runs quite slow and in the end it prints the individual with the highest fitness, the gene that has the highest win precentage after
+10 generations. It has been showed when testing the solution that there might be a problem using only 2 rules and a random strategy. That leads that both players
+starts with random movement and then only in the end practice their rules. So to improve, more rules should be implemented which is possible with the current deisgn.
+Should also be less random which opponent to face. A gene could be able to try its luck against bad strategies, opotimal and OK ones.
+
+
+### 3.3 An agent using minmax
+
+### 3.4 An agent using reinforcement learning
--- lab3/test.py
+++ lab3/test.py
@@ -1,10 +1,16 @@
-import logging
 import random
 from itertools import accumulate
 from operator import xor
 from copy import deepcopy
 from collections import namedtuple
+from tqdm import tqdm
 
+# Some global variables used to control population and game
+POPULATION_SIZE = 100
+NR_OF_ROWS = 11
+OFFSPRING_SIZE = 50
+MUTATION_PROB = 0.3
+NR_OF_GENERATIONS = 10
 
 Nimply = namedtuple("Nimply", "row, num_objects")
 
@@ -35,6 +41,48 @@ class Nim:
         self._rows[row] -= num_objects
 
 
+class Gene:
+    """
+        Gene class used in population. It holds information about the gene and how its performed. Also holds a strategy
+        for making a move and a setter for that strategy
+    """
+
+    def __init__(self):
+        self.parameters = self.populate()
+        self.strategy = RandRule()
+        self.games_played = 0
+        self.games_won = 0
+        self.score = 0
+
+    def update_score(self, win):
+        if win:
+            self.games_won += 1
+        self.games_played += 1
+        self.score = self.games_won / self.games_played
+
+    def populate(self):
+        """
+            key : strategy name
+            value : how many objects to leave on each row
+        """
+        parameters = {'one_row_left': random.randint(1, NR_OF_ROWS),
+                      'two_rows_left': random.randint(1, NR_OF_ROWS)}
+        return parameters
+
+    def set_strategy(self, new_strategy):
+        """
+            setter for strategy method
+            :param new_strategy: Callable, strategy to execute when calling execute()
+        """
+        self.strategy = new_strategy
+
+    def execute(self, move):
+        """
+            Call depending on dynamic "configuration", we execute move using a strategy. Default random.
+        """
+        return self.strategy.move(move, self.parameters)
+
+
 class Strategy:
     """
         Abstract strategy class, defines move interface for specific implementations
@@ -53,75 +101,69 @@ class Strategy:
 
 class OneRowLeft(Strategy):
     """
-        Strategy to use to win the game when the game only consists x units on one single row
+        Strategy to leave x tiles in game when only one row is left
     """
 
     def __init__(self):
         super().__init__()
 
-    def move(self, move, parameters):
-        print('movin with rule 1')
-        return Nimply(move[0], move[1])
+    def move(self, possible_moves, parameters):
+        print('moving with rule 1')
+        max_move = max(possible_moves, key=lambda i: i[1])
+        units = max(max_move[1] - parameters['one_row_left'], 1)
+        return Nimply(max_move[0], units)
 
 
 class RandRule(Strategy):
     """
-        Strategy to use to win the game when the game only consists x units on one single row
+        Pure random strategy, used as default for all agents
     """
 
     def __init__(self):
         super().__init__()
 
-    def move(self, move, parameters):
+    def move(self, possible_moves, parameters):
         print('im moving with a random strategy')
+        move = possible_moves[random.randint(0, len(possible_moves) - 1)]
         return Nimply(move[0], move[1])
 
 
-class Gene:
+class TwoRowsLeft(Strategy):
+    """
+        Apply when there are two rows left. Then take x amount of elements from the row with most tiles left
+    """
+
     def __init__(self):
-        self.parameters = self.populate()
-        self.strategy = RandRule()
+        super().__init__()
+
+    def move(self, possible_moves, parameters):
+        # leave x amount of elements from the row with most tiles left
+        print('Using two row strategy')
+        max_move = max(possible_moves, key=lambda i: i[1])
+        nr_of_tiles = max(max_move[1] - parameters['two_rows_left'], 1)
+        return Nimply(max_move[0], nr_of_tiles)
 
 
-    def populate(self):
-        """
-            key : strategy name
-            value : how many objects to leave on each row
-        """
-        parameters = {'one_row_left': random.randint(1, NR_OF_ROWS),
-                      'one_row_to_win': random.randint(1, NR_OF_ROWS)}
-        return parameters
-
-    def set_strategy(self, new_strategy):
-        """
-            setter for strategy method
-            :param new_strategy: Callable, strategy to execute when calling execute()
-        """
-        self.strategy = new_strategy
-
-    def execute(self, move):
-        """
-            Call depending on dynamic "configuration", we execute move using a strategy. Default random.
-        """
-        return self.strategy.move(move, self.parameters)
-
-
+# Provided by professor, used for agent playing nim sum strategy
 def nim_sum(state: Nim) -> int:
     *_, result = accumulate(state.rows, xor)
     return result
 
 
+# Random strategy used in part 3.1
 def pure_random(state: Nim) -> Nimply:
     move_dict = cook_status(state)
     row, nr = move_dict['possible_moves'][random.randint(0, len(move_dict['possible_moves']) - 1)]
     return Nimply(row, nr)
 
 
+# Provided from professor with some small changes
 def optimal_strategy(state: Nim) -> Nimply:
     data = cook_status(state)
     return next((bf for bf in data["brute_force"] if bf[1] == 0), random.choice(data["brute_force"]))[0]
 
 
+# Provided from professor with some small changes
 def cook_status(state: Nim) -> dict:
     cooked = dict()
     cooked["possible_moves"] = [
@@ -156,19 +198,11 @@ def play_nim(game_nim: Nim):
     print(f'player {1 - player} won')
 
 
-# game = Nim(11)
-# play_nim(game)
-
-
-# LAB 3.2 BELOW
-
-# Some global variables used to control population and game
-POPULATION_SIZE = 100
-NR_OF_ROWS = 11
-
-
-def play_nim_game(game_nim, agent):
-    players = [agent[0], agent[1]]
+def play_nim_game(game_nim, agent1, agent2):
+    """
+        Playing game using agents with different strategies
+    """
+    players = [agent1, agent2]
     index = 0
     print(f'initial state: {game_nim}')
     while game_nim:
@@ -177,37 +211,96 @@ def play_nim_game(game_nim, agent):
         print(f'Board after player {players[index]}\'s move: {game_nim}')
         index = 1 - index
     print(f'player {1 - index} won')
+    winner = players[1 - index]
+    loser = players[index]
+    return winner, loser
 
 
 def evaluate_game(state, gene):
-    # get information from game
+    """
+        get information from game and call strategies
+    """
     game_dict = cook_status(state)
 
-    # evaluate information based on most important rules first, return Nimply from gene class
-
-    # TODO: FIX TO NOT TAKE TO MANY BRICKS
     if game_dict['active_rows_number'] == 1:
-        move = (game_dict['last_row'], gene.parameters['one_row_left'])
-        #tmp = [(1,2), (2,6), (5,4)]
-        #test = max(tmp, key=lambda i: i[1])
-        biggest_move = max(game_dict['possible_moves'], key=lambda i: i[1])
-        if biggest_move[1] < gene.parameters['one_row_left']:
-            move = (game_dict['last_row'], biggest_move[1])
         gene.set_strategy(OneRowLeft())
-        return gene.execute(move)
+        return gene.execute(game_dict['possible_moves'])
 
-    # elif game_dict['active_rows_number'] == 2:
-    #    pass
+    elif game_dict['active_rows_number'] == 2:
+        gene.set_strategy(TwoRowsLeft())
+        return gene.execute(game_dict['possible_moves'])
+
     else:
-        move = game_dict['possible_moves'][random.randint(0, len(game_dict['possible_moves']) - 1)]
         gene.set_strategy(RandRule())
-        ply = gene.execute(move)
+        ply = gene.execute(game_dict['possible_moves'])
         return ply
 
 
+def tournament(p1, p2):
+    if p1.score > p2.score:
+        return p1
+    else:
+        return p2
 
-game = Nim(NR_OF_ROWS)
-gene1 = Gene()
-gene2 = Gene()
 
-play_nim_game(game, [gene1, gene2])
+def create_offspring(population):
+    for i in range(OFFSPRING_SIZE):
+        tmp = random.sample(population, 4)
+        parent1 = tournament(tmp[0], tmp[2])
+        parent2 = tournament(tmp[1], tmp[3])
+        new_gene = crossover(parent1, parent2)
+        population.append(new_gene)
+    return population
+
+
+def crossover(p1, p2):
+    first_val = p1.parameters['one_row_left']
+    second_val = p2.parameters['two_rows_left']
+    child = Gene()
+    child.parameters['one_row_left'] = first_val
+    child.parameters['two_rows_left'] = second_val
+    return child
+
+
+def mutate(population):
+    rand_index = random.randint(0, len(population) - 1)
+    gene_to_mutate = population[rand_index]
+    gene_to_mutate.parameters['one_row_left'] = random.randint(0, NR_OF_ROWS)
+    gene_to_mutate.parameters['two_rows_left'] = random.randint(0, NR_OF_ROWS)
+    population.append(gene_to_mutate)
+    return population
+
+
+def main():
+    # uncomment this to run part 3.2
+    # game = Nim(11)
+    # play_nim(game)
+
+    # init population
+    population = []
+    for _ in range(POPULATION_SIZE):
+        population.append(Gene())
+
+    for _ in tqdm(range(NR_OF_GENERATIONS)):
+        # play games with genes vs genes (not facing each other) and save score.
+        for gene in population:
+            k = 0
+            while k < 10:
+                rand_opponent = population[random.randint(0, len(population) - 1)]
+                if rand_opponent is not gene:
+                    game = Nim(NR_OF_ROWS)
+                    winner, loser = play_nim_game(game, gene, rand_opponent)
+                    winner.update_score(True)
+                    loser.update_score(False)
+                    k += 1
+
+        population = create_offspring(population)
+        if random.random() < MUTATION_PROB:
+            population = mutate(population)
+        population = sorted(population, key=lambda x: x.score, reverse=True)[:POPULATION_SIZE]
+
+    print(f'best gene: {population[0].parameters}, score {population[0].score}')
+
+
+if __name__ == "__main__":
+    main()
